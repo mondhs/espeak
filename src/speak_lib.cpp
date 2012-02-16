fa7634e5d7ea0b224ad7d3b282b97fec7accaae0
Reece Dunn <msclrhd@gmail.com>
2012-02-16 19:52:46 +0000

    out_samplerate is not/no longer used, so remove it

--- src/speak_lib.cpp
+++ src/speak_lib.cpp
@@ -51,7 +51,6 @@ void* my_audio=NULL;
 
 static void* my_user_data=NULL;
 static espeak_AUDIO_OUTPUT my_mode=AUDIO_OUTPUT_SYNCHRONOUS;
-static int out_samplerate = 0;
 static int voice_samplerate = 22050;
 
 t_espeak_callback* synth_callback = NULL;
