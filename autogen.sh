2fd15ea0fd33f0b8f1e3d8cda7cbc661dc41453e
Mondhs Bus <mondhs@github.com>
2014-02-11 13:00:25 +0200

    migrating to espeak 1.48

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir -p m4
+touch AUTHORS
+touch NEWS
+ln -svf README.md README
+
+aclocal -I m4 || exit 1
+
+libtoolize || exit 1
+autoheader || exit 1
+automake --add-missing || exit 1
+autoconf || exit 1
